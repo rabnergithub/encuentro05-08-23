bf70e695783c0d48216ce9c3b30283a751e8bb3d
Rabner <est.aramirezj@uml.edu.ni>
2023-08-05 12:35:57 -0600

    peticiones api

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import { Title } from '../components/Title';
 
 export const Inicio = () => {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))// Update the document title using the browser API
+  });
   return (
     <>
       <Title titulo="Bienvenido!" />
